4c9c1452385feef3529fc670bb21ee8982076c28
amnazu <amna_zukic@hotmail.com>
2021-04-02 03:00:13 +0200

    Dvadeset peti dan

---
+++ ex05/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PostRequest from './PostRequest.js';
+
+function App() {
+  return (
+    <div className="App">
+    <PostRequest />
+    </div>
+  );
+}
+
+export default App;
